fa1f55c25895b01b1cf2aaecbf2083ab2741182f
Maciej Jaszczura <211808@edu.p.lodz.pl>
2020-06-08 23:29:28 +0200

    Generated controllers with WebAPI methods.
    Following classes were created:
    - ClassesController
    - CustomersController
    - ReservationsController
    - SchedulesController
    - TrainersController

---
+++ GymApplication/Controllers/ClassesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GymApplication.DAL;
+using GymApplication.Models;
+
+namespace GymApplication.Controllers
+{
+    public class ClassesController : ApiController
+    {
+        private GymDbContext db = new GymDbContext();
+
+        // GET: api/Classes
+        public IQueryable<Classes> GetClasses()
+        {
+            return db.Classes;
+        }
+
+        // GET: api/Classes/5
+        [ResponseType(typeof(Classes))]
+        public async Task<IHttpActionResult> GetClasses(int id)
+        {
+            Classes classes = await db.Classes.FindAsync(id);
+            if (classes == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(classes);
+        }
+
+        // PUT: api/Classes/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutClasses(int id, Classes classes)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != classes.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(classes).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClassesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Classes
+        [ResponseType(typeof(Classes))]
+        public async Task<IHttpActionResult> PostClasses(Classes classes)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Classes.Add(classes);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = classes.ID }, classes);
+        }
+
+        // DELETE: api/Classes/5
+        [ResponseType(typeof(Classes))]
+        public async Task<IHttpActionResult> DeleteClasses(int id)
+        {
+            Classes classes = await db.Classes.FindAsync(id);
+            if (classes == null)
+            {
+                return NotFound();
+            }
+
+            db.Classes.Remove(classes);
+            await db.SaveChangesAsync();
+
+            return Ok(classes);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClassesExists(int id)
+        {
+            return db.Classes.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ GymApplication/Controllers/ReservationsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GymApplication.DAL;
+using GymApplication.Models;
+
+namespace GymApplication.Controllers
+{
+    public class ReservationsController : ApiController
+    {
+        private GymDbContext db = new GymDbContext();
+
+        // GET: api/Reservations
+        public IQueryable<Reservations> GetReservations()
+        {
+            return db.Reservations;
+        }
+
+        // GET: api/Reservations/5
+        [ResponseType(typeof(Reservations))]
+        public async Task<IHttpActionResult> GetReservations(int id)
+        {
+            Reservations reservations = await db.Reservations.FindAsync(id);
+            if (reservations == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reservations);
+        }
+
+        // PUT: api/Reservations/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutReservations(int id, Reservations reservations)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != reservations.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(reservations).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservationsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Reservations
+        [ResponseType(typeof(Reservations))]
+        public async Task<IHttpActionResult> PostReservations(Reservations reservations)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Reservations.Add(reservations);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = reservations.ID }, reservations);
+        }
+
+        // DELETE: api/Reservations/5
+        [ResponseType(typeof(Reservations))]
+        public async Task<IHttpActionResult> DeleteReservations(int id)
+        {
+            Reservations reservations = await db.Reservations.FindAsync(id);
+            if (reservations == null)
+            {
+                return NotFound();
+            }
+
+            db.Reservations.Remove(reservations);
+            await db.SaveChangesAsync();
+
+            return Ok(reservations);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ReservationsExists(int id)
+        {
+            return db.Reservations.Count(e => e.ID == id) > 0;
+        }
+    }
+}
