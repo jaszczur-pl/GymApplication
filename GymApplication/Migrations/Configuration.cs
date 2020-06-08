2b66a3e1f6616ee73b3224a911518052c033f940
Maciej Jaszczura <211808@edu.p.lodz.pl>
2020-06-09 00:07:44 +0200

    Provided test data in Seed method for each database entity

--- GymApplication/Migrations/Configuration.cs
+++ GymApplication/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿namespace GymApplication.Migrations
 {
+    using GymApplication.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,10 +15,35 @@
 
         protected override void Seed(GymApplication.DAL.GymDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            context.Classes.AddOrUpdate(x => x.ID,
+            new Classes() { ID = 1, Name = "Fit bike", UsersLimit = 15 },
+            new Classes() { ID = 2, Name = "Zumba", UsersLimit = 20 },
+            new Classes() { ID = 3, Name = "Crossfit", UsersLimit = 10 },
+            new Classes() { ID = 4, Name = "Kalistenika", UsersLimit = 10 },
+            new Classes() { ID = 5, Name = "Trening obwodowy", UsersLimit = 10 });
+
+            context.Trainers.AddOrUpdate(x => x.ID,
+            new Trainer() { ID = 1, Name = "Jan", Surname = "Kowalski" },
+            new Trainer() { ID = 2, Name = "Michał", Surname = "Nowak" },
+            new Trainer() { ID = 3, Name = "Julia", Surname = "Adamska" });
+
+            context.Customers.AddOrUpdate(x => x.ID,
+            new Customer() { ID = 12345678, Name = "Stanisław", Surname = "Regulski", Email = "stanislaw.regulski@gmail.com", Password = "sregulski" },
+            new Customer() { ID = 21231231, Name = "Joanna", Surname = "Nowak", Email = "joanna.nowak@gmail.com", Password = "jnowak"},
+            new Customer() { ID = 33213213, Name = "Krzysztof", Surname = "Jędrzejczak", Email = "krzysztof.jedrzejczak@gmail.com", Password = "kjedrzejczak" });
+
+            context.Schedules.AddOrUpdate(x => x.ID,
+            new Schedule() { ID = 1, ClassID = 1, TrainerID = 1, DateFrom = new DateTime(2020, 8, 10, 10, 0, 0), DateTo = new DateTime(2020, 8, 10, 11, 0, 0), NumberOfAvailablePlaces = 15 },
+            new Schedule() { ID = 2, ClassID = 2, TrainerID = 2, DateFrom = new DateTime(2020, 8, 11, 12, 0, 0), DateTo = new DateTime(2020, 8, 11, 13, 0, 0), NumberOfAvailablePlaces = 20 },
+            new Schedule() { ID = 3, ClassID = 3, TrainerID = 3, DateFrom = new DateTime(2020, 8, 12, 17, 0, 0), DateTo = new DateTime(2020, 8, 12, 18, 0, 0), NumberOfAvailablePlaces = 10 });
+
+            context.Reservations.AddOrUpdate(x => x.ID,
+            new Reservations() { ID = 1, CustomerID = 12345678, ScheduleID = 1 },
+            new Reservations() { ID = 2, CustomerID = 21231231, ScheduleID = 2 },
+            new Reservations() { ID = 3, CustomerID = 33213213, ScheduleID = 3 });
+
+            context.SaveChanges();
         }
     }
 }
